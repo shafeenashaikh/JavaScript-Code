f1feead307d032f6d4c0566a17d201a37a46665b
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-12-03 16:07:14 +0530

    if else conditions in js

---
+++ Condition.js
@@ -0,0 +1,15 @@
+let num1 = 6;
+let num2 = 9;
+let num3 = 7;
+
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is greater");
+}
+    
+else if(num2 > num1 && num2 > num3){
+    console.log("num2 is greater");
+}
+else{
+    console.log("num3 is greatest")
+}
+console.log("Bye");
