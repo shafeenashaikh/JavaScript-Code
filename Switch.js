54da6c7acd686c41bf583e43cb38965b20694f44
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-12-03 16:40:27 +0530

    Switch statments in js

---
+++ Switch.js
@@ -0,0 +1,21 @@
+let day = "Monday"
+
+switch(day){
+    case 'Monday':
+        console.log("7am");
+        break;
+    case 'Tuesday':
+        console.log("4am");
+    case 'Wedensday':
+        console.log("4am");
+    case 'Thursday':
+        console.log("4am");
+    case 'Friday':
+        console.log("5am");
+    case 'Saturday':
+        console.log("6am");
+    case 'Sunday':
+        console.log("8am");
+    default:
+        console.log("sleep")
+}
